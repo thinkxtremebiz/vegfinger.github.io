47b88c65a5e98794b8616c8deeaaee71ed995623
TeslaLord <ntharun@gmail.com>
2020-10-23 17:08:49 +0530

    some js

--- js/main.js
+++ js/main.js
@@ -20,7 +20,27 @@ jQuery(document).ready(function ($) {
     },
     speed: 400
   });
+  var num = 1,
+    price = 7;
 
+  $(".fa-plus").on("click", function () {
+
+    var _num = parseInt($("span.number").text()) + 1;
+    $("span.number").text(_num);
+    updatePrice(_num, price);
+  });
+
+  $(".fa-minus").on("click", function () {
+
+    var _num = parseInt($("span.number").text()) - 1;
+    if (_num > -1)
+      $("span.number").text(_num);
+    updatePrice(_num, price);
+  });
+
+  function updatePrice(num, price) {
+    $(".amount").text(num * price);
+  }
   // Mobile Navigation
   if ($('#nav-menu-container').length) {
     var $mobile_nav = $('#nav-menu-container').clone().prop({
